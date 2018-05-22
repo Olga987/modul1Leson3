335de02da9a4769bbd27a71260df4d501627b610
ГерценЕ <@238_TEACHER>
2018-05-22 10:00:31 +0600

    Add project files.

---
+++ ConsoleApplication2/Source.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a = 0, S = 0.0;
+	printf("¬ведите значение стороны квадрата а: ");
+	scanf("%f", &a);
+
+	S = a*a;
+	printf("S=%f\n", S);
+
+
+
+
+
+
+
+	/*int x = 0, y = 0;
+
+	printf("¬ведите значение x: ");
+	scanf("%d", &x);
+
+	y = 7 * (x*x) - (3 * x) + 6;
+
+		printf("y=%d\n", y);*/
+
+
+
+
+		/*
+			printf("5 10\t 100\t x25\n");
+			printf("7cm\t 1949v\t xy\n");
+		*/
+}
